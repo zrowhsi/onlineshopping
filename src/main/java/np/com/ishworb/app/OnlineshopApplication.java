2ceb81e4fca24277db11ea0260ef3ffcaf163a73
Ishwor Budhathoki <zrowhsi@gmail.com>
2018-04-24 16:07:52 +0545

    inital commit

---
+++ src/main/java/np/com/ishworb/app/OnlineshopApplication.java
@@ -0,0 +1,12 @@
+package np.com.ishworb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopApplication.class, args);
+	}
+}
